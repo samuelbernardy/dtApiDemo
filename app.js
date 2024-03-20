5c9809ea312ab415ab0a697349ab421fad4f4465
Samuel Bernardy <samuel.bernardy@gmail.com>
2024-03-20 16:44:37 -0400

    cleaner job just for key user action cleanup.

--- app.js
+++ app.js
@@ -4,110 +4,31 @@ const app = express();
 const port = 3000;
 // import utils
 const {
-  downloadResource,
-  getListOfMonitorIds,
-  getListOfMZIds,
-  buildMZIterable,
-  buildIterable,
-  monitorTagFormat,
   testToken,
-  buildExecutionIterable,
-  filterMZ,
-  createMZ,
+  getApplicationMethods,
+  deleteApplicationMethods,
 } = require("./utils.js");
-//const testData = require("./testMzs.json");
-//EXPORT SYNTHETIC DETAIL CSV
-app.get("/exportMonitorsDetail", (req, res) => {
-  let params = [];
 
-  for (const [key, value] of Object.entries(req.query)) {
-    params.push({ label: `${key}`, value: `${value}` });
-  }
+// ---------- KEY USER ACTION CLEANUP ---------- //
 
-  isParams = params.length > 0;
-
-  if (!isParams) {
-    res.send(
-      "declare columns and datasource in the query parameters. Example can be found here: https://github.com/samuelbernardy/dtApiDemo/blob/master/readme.md#exportmonitorsdetail"
-    );
-  } else {
-    getListOfMonitorIds()
-      .then((list) => {
-        return buildIterable(list);
-      })
-      .then((detailsIterable) => {
-        Promise.all(detailsIterable).then((data) => {
-          downloadResource(
-            res,
-            "syntheticMonitors" + Date.now() + ".csv",
-            params == [] ? null : params,
-            monitorTagFormat(data)
-          );
-        });
-      });
-  }
-});
-//EXPORT EXECUTION DETAILS CSV
-app.get("/exportExecutionDetails", (req, res) => {
-  let params = { monitors: [], resultType: "" };
-
-  for (const [key, value] of Object.entries(req.query)) {
-    if (key.indexOf("exec") > -1) {
-      params.monitors.push(`${value}`);
-    } else if (key == "resultType") {
-      params.resultType = `${value}`;
-    }
-  }
-  isParams =
-    params.monitors.length > 0 &&
-    (params.resultType == "SUCCESS" || params.resultType == "FAILED");
-  if (!isParams) {
-    res.send("Parameters must be set.");
-  } else {
-    getListOfMonitorIds()
-      .then((list) => {
-        console.log("oldList", list);
-        let newList = list.filter((entity) => params.monitors.includes(entity));
-        console.log("newList", newList);
-        return buildExecutionIterable(newList);
-      })
-      .then((detailsIterable) => {
-        Promise.all(detailsIterable).then((data) => {
-          console.log("execData", data);
-          downloadResource(
-            res,
-            "syntheticExecutions" + Date.now() + ".csv",
-            null,
-            data
-          );
-        });
-      });
-  }
-});
-
-//UPGRADE MANAGEMENT ZONES TO INCLUDE FOCUS GROUPS
-app.get("/upgradeMZs", (req, res) => {
-  console.log("get list of MZs");
-  // get all mzs
-  getListOfMZIds().then((list) => {
-    // assembling iterable out of get details calls
-    const iterable = buildMZIterable(list);
-    // execute get details calls
-    Promise.all(iterable).then((data) => {
-      // Filter data by Synthetic entity
-      // USE FOLLOWING FOR LIMITED TESTING
-      // const filtered = filterMZ(testData);
-      const filtered = filterMZ(testData);
-      // iterate through mz
-      filtered.forEach((mz) => {
-        //create new mzs
-        createMZ(mz);
-      });
-      // print filtered data payload to browser window
-      res.send(filtered);
-    });
+//GET LIST OF KUAS
+app.get("/getApplicationMethods", (req, res) => {
+  getApplicationMethods().then((data) => {
+    console.log(data);
+    res.send(data);
   });
 });
+
+//DELETE KUAS
+app.get("/deleteApplicationMethods", (req, res) => {
+  getApplicationMethods().then((data) => {
+    console.log(data);
+    deleteApplicationMethods(data.entities);
+    res.send(data);
+  });
+});
+
+// ---------- SERVER CHECK METHODS ---------- //
 
 //TEST SERVER
 app.get("/", (req, res) => {
--- utils.js
+++ utils.js
@@ -3,191 +3,66 @@
 const dynatraceApiClient = require("@dt-esa/dynatrace-api-client");
 const { DynatraceTenantAPI } = require("@dt-esa/dynatrace-api-client");
 const { application } = require("express");
-// import csv handler
-const { Parser } = require("json2csv");
 // build api client
 const apiConfig = require("./apiConfig.json");
-const dtAPI = new DynatraceTenantAPI(
-  {
-    url: apiConfig.tenant,
-    token: apiConfig.token,
-  },
-  false
-);
-//GET ALL DETAILS
-function getDetailsForEachMonitor(entityId) {
-  console.log("getting details for " + entityId);
-  return new Promise((resolve, reject) => {
-    dtAPI.v1.synthetic.getMonitor(entityId).then((data) => {
-      resolve(data);
-    });
-  });
-}
-//GET DETAILS FOR EACH MZ
-function getDetailsForEachMZ(entityId) {
-  console.log("getting details for " + entityId);
-  return new Promise((resolve, reject) => {
-    dtAPI.config.managementZones.getManagementZone(entityId).then((data) => {
-      resolve(data);
-    });
-  });
-}
-//GET Executions for each monitor
-function getExecutionsForEachMonitor(entityId, resultType) {
-  console.log("getting executions details for " + entityId);
-  return new Promise((resolve, reject) => {
-    dtAPI.v2.synthetic
-      .getExecutionResult(entityId, resultType)
-      .then((data) => {
-        resolve(data);
-      })
-      .catch((e) => resolve(console.log(e)));
-  });
-}
+const dtAPI = new DynatraceTenantAPI({
+  url: apiConfig.tenant,
+  token: apiConfig.token,
+});
+
 module.exports = {
-  // ----------- START SYNTHETICS EXPORT MODULES ----------- //
+  // ---------- KEY USER ACTION CLEANUP ---------- //
 
-  // INIT DOWNLOAD
-  downloadResource: function (res, fileName, fields, data) {
-    console.log("generating csv " + fileName);
-    const json2csv = new Parser({ fields });
-    const csv = json2csv.parse(data);
-    res.header("Content-Type", "text/csv");
-    res.attachment(fileName);
-    console.log("sending attachment");
-    return res.send(csv);
-  },
-  //GET MONITORS
-  getListOfMonitorIds: function () {
-    console.log("getting list of monitors");
+  getApplicationMethods: function () {
+    console.log("getting list of apps");
     return new Promise((resolve, reject) => {
-      dtAPI.v1.synthetic.getMonitorsCollection().then((data) => {
-        const map = data.monitors.map((entity) => entity.entityId);
-        resolve(map);
-      });
-    });
-  },
-  //CREATE DETAIL PROMISE ARRAY
-  buildIterable: function (list) {
-    console.log("building promise array");
-    let detailCalls = [];
-    list.forEach((entityId) => {
-      detailCalls.push(getDetailsForEachMonitor(entityId));
-    });
-    return detailCalls;
-  },
-  //REFORMAT TAG FIELDS
-  monitorTagFormat: function (obj) {
-    console.log("pivoting tags object");
-    obj
-      // Filter out all entitys that have no tags
-      .filter((entity) => entity.tags && entity.tags.length > 0)
-      // For each entity with a tag
-      .forEach((entity) => {
-        entity.tags.forEach((t) => {
-          const split = t.key.split(":");
-          const isKVkey = t.key.includes(":");
-
-          const name = isKVkey ? split[0] : t.name;
-          const value = isKVkey ? split[1] : t.value;
-
-          entity[
-            `tags.${t.context !== "CONTEXTLESS" ? `[${t.context}]` : ""}${name}`
-          ] = value;
-        });
-        // remove original json
-        entity.tags = undefined;
-      });
-    return obj;
-  },
-
-  // -----------  END  SYNTHETICS EXPORT MODULES ----------- //
-
-  // -----------  START MZ MGMT MODULES ----------- //
-
-  //GET LIST OF MANAGEMENT ZONES
-  getListOfMZIds: function () {
-    console.log("getting list of management zones");
-    return new Promise((resolve, reject) => {
-      dtAPI.config.managementZones.listManagementZones().then((data) => {
-        const map = data.values.map((entity) => entity.id);
-        resolve(map);
-      });
-    });
-  },
-  //BUILD MZ Data Iterable
-  buildMZIterable: function (list) {
-    console.log("getting list of filtered mz data");
-    let detailCalls = [];
-    list.forEach((entityId) => {
-      // https://www.dynatrace.com/support/help/shortlink/api-config-management-zones-get-all
-      detailCalls.push(getDetailsForEachMZ(entityId));
-    });
-    return detailCalls;
-  },
-  //FILTER MZs
-  filterMZ: function (payload) {
-    console.log("payload\n" + JSON.stringify(payload));
-    // determine types for sub mz creation
-    const types = ["BROWSER_MONITOR", "HTTP_MONITOR"];
-    payload.forEach((mz) => {
-      // remove non-synethtic types
-      newRules = mz.rules.filter(
-        (r) => r.type == types[0] || r.type == types[1]
-      );
-      mz.rules = newRules;
-    });
-    // remove mzs without remaining rules
-    const cleanPayload = payload.filter((mz) => mz.rules.length > 1);
-    console.log("clean payload\n" + JSON.stringify(cleanPayload));
-    return cleanPayload;
-  },
-  //CREATE MZs
-  createMZ: function (filteredMZ) {
-    return new Promise((resolve, reject) => {
-      console.log("this is the filtered payload\n" + filteredMZ);
-      //build new mz
-      let mzFormatted = {
-        metadata: null,
-        id: null,
-        name: "Synthetic_" + filteredMZ.name,
-        description:
-          "Synthetic administration group for MZ: " + filteredMZ.name,
-        rules: filteredMZ.rules,
-        dimensionalRules: filteredMZ.ruldimensionalRuleses,
-        entitySelectorBasedRules: filteredMZ.entitySelectorBasedRules,
-      };
-      // https://www.dynatrace.com/support/help/shortlink/api-config-management-zones-post-mz
-      dtAPI.config.managementZones.createManagementZone(mzFormatted);
-    });
-  },
-
-  // -----------  END MZ MGMT MODULES ----------- //
-
-  // -----------  START TEST EXPORT MODULES ----------- //
-
-  testToken: function () {
-    return new Promise((resolve, reject) => {
-      data = {
-        token: apiConfig.token,
-      };
-      dtAPI.v2.apiTokens
-        .lookupApiToken(data)
-        .then((info) => {
-          resolve(info);
+      dtAPI.v2.entities
+        .getEntities({
+          entitySelector: "type(APPLICATION_METHOD)",
+          pageSize: 800,
+          fields: "fromRelationships, lastSeenTms",
+          from: "now-365d",
         })
-        .catch((e) => {
-          resolve(e);
+        .then((data) => {
+          console.log(data);
+          resolve(data);
         });
     });
   },
-  //CREATE EXECUTION PROMISE ARRAY
-  buildExecutionIterable: function (list, resultType) {
-    console.log("building execution promise array");
-    let detailCalls = [];
-    list.forEach((entityId) => {
-      detailCalls.push(getExecutionsForEachMonitor(entityId, resultType));
+
+  //DELETE LIST OF KUAs
+  deleteApplicationMethods: function (list) {
+    console.log("deleting list of kuas");
+    let kuasToDelete = [];
+    let deleteCalls = [];
+    let now = new Date();
+    let cutOffDate = now - 1000 * 60 * 60 * 24 * 180;
+    console.log(new Date(cutOffDate));
+    list.forEach((kua) => {
+      console.log(new Date(kua.lastSeenTms));
+      if (
+        kua.lastSeenTms <= cutOffDate &&
+        kua.fromRelationships.isApplicationMethodOf
+      ) {
+        console.log(kua);
+        let idleTime = now - kua.lastSeenTms;
+        kuasToDelete.push({
+          userActionId: kua.entityId,
+          name: kua.displayName,
+          parentApp: kua.fromRelationships.isApplicationMethodOf[0].id,
+          idleTime: idleTime / (1000 * 60 * 60) + " hours",
+        });
+      }
     });
-    return detailCalls;
+    console.log("KEY USER ACTIONS TO CLEAN UP: \n\n", kuasToDelete);
+    // kuasToDelete.forEach((kua) => {
+    //   new Promise((resolve, reject) => {
+    //     dtAPI.config.applications
+    //       .deleteKeyUserAction(kua.parentApp, kua.userActionId)
+    //       .then((data) => {
+    //         resolve(data);
+    //       });
+    //   });
+    // });
   },
 };
